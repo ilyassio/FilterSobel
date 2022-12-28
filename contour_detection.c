57b1dd6d05c982507622bd3e50d7aeabe1e713b0
Ilyas <ilmakroum@gmail.com>
2022-12-28 20:40:24 +0100

    Various improvements

--- contour_detection.c
+++ contour_detection.c
@@ -2,42 +2,59 @@
 #include <math.h>
 #include "input_matrix.h"
 
-int main() {
+#define threshold 250
 
-    int fx[3][3] = {{-1, 0, 1}, {-2, 0, 2}, {-1, 0, 1}};
-    int fy[3][3] = {{-1, -2, -1}, {0, 0, 0}, {1, 2, 1}};
+int main()
+{
 
-    int x_size = 472;
-    int y_size = 473;
+    int Gx[3][3] = {{-1, 0, 1}, {-2, 0, 2}, {-1, 0, 1}};
+    int Gy[3][3] = {{-1, -2, -1}, {0, 0, 0}, {1, 2, 1}};
 
-    int X[x_size-2][y_size-2];
-    int Y[x_size-2][y_size-2];
-    int F[x_size-2][y_size-2];
+    int x_size = sizeof(img[0]) / sizeof(img[0][0]);
+    int y_size = sizeof(img) / sizeof(img[0]);
 
-    for(int x = 0; x < x_size-2; x++) {
-        for(int y = 0; y < y_size-2; y++) {
-            X[x][y] = img[x][y] * fx[0][0] + img[x][y+1] * fx[0][1] + img[x][y+2] * fx[0][2] + img[x+1][y] * fx[1][0] + img[x+1][y+1] * fx[1][1] + img[x+1][y+2] * fx[1][2] + img[x+2][y] * fx[2][0] + img[x+2][y+1] * fx[2][1] + img[x+2][y+2] * fx[2][2];
+    int X, Y, x, y;
+    int finalImage[x_size - 2][y_size - 2];
+
+    for (x = 0; x < x_size - 2; x++)
+    {
+        for (y = 0; y < y_size - 2; y++)
+        {
+            X = img[x][y] * Gx[0][0] + img[x][y + 1] * Gx[0][1] + img[x][y + 2] * Gx[0][2] + img[x + 1][y] * Gx[1][0] + img[x + 1][y + 1] * Gx[1][1] + img[x + 1][y + 2] * Gx[1][2] + img[x + 2][y] * Gx[2][0] + img[x + 2][y + 1] * Gx[2][1] + img[x + 2][y + 2] * Gx[2][2];
+            Y = img[x][y] * Gy[0][0] + img[x][y + 1] * Gy[0][1] + img[x][y + 2] * Gy[0][2] + img[x + 1][y] * Gy[1][0] + img[x + 1][y + 1] * Gy[1][1] + img[x + 1][y + 2] * Gy[1][2] + img[x + 2][y] * Gy[2][0] + img[x + 2][y + 1] * Gy[2][1] + img[x + 2][y + 2] * Gy[2][2];
+            finalImage[x][y] = sqrt((X*X) + (Y*Y)) < threshold ? 0 : 255;
         }
     }
+    
+    // Save image
 
-    for(int y = 0; y < y_size-2; y++) {
-        for(int x = 0; x < x_size-2; x++) {
-            Y[x][y] = img[x][y] * fy[0][0] + img[x][y+1] * fy[0][1] + img[x][y+2] * fy[0][2] + img[x+1][y] * fy[1][0] + img[x+1][y+1] * fy[1][1] + img[x+1][y+2] * fy[1][2] + img[x+2][y] * fy[2][0] + img[x+2][y+1] * fy[2][1] + img[x+2][y+2] * fy[2][2];
+    FILE *pimg;
+    pimg = fopen("pgmimg.pgm", "wb");
+
+    // writing  the Magic Number into the file
+    fprintf(pimg, "P2\n");
+
+    // width and height writing into the file
+    int w = x_size;
+    int h = y_size;
+    fprintf(pimg, "%d %d\n", w, h);
+
+    // Max grey color value
+    fprintf(pimg, "255\n");
+    int cnt = 0;
+    int tp = 0;
+    for (int i = 0; i < h; i++)
+    {
+        for (int j = 0; j < w; j++)
+        {
+            tp = finalImage[i][j];
+
+            // 2D array grey value into file
+            fprintf(pimg, "%d ", tp);
         }
+        fprintf(pimg, "\n");
     }
+    fclose(pimg);
 
-    for(int x = 0; x < x_size-2; x++) {
-        for(int y = 0; y < y_size-2; y++) {
-            F[x][y] = sqrt((X[x][y] * X[x][y]) + (Y[x][y] * Y[x][y]));
-        }
-    }   
-
-    printf("Hello Ilyas\n");
-    printf("10: %d\n", F[10][10]);
-    printf("100: %d\n", F[100][100]);
-    printf("150: %d\n", F[150][150]);
-    printf("200: %d\n", F[200][200]);
-    printf("250: %d\n", F[250][250]);
-    printf("300: %d\n", F[300][300]);
     return 0;
 }
